740bcc73c181078eca25d956e5f0a338a7c42fb6
teydd <chemosted@gmail.com>
2026-06-25 23:59:47 +0300

    update

--- lib/actions/customer.ts
+++ lib/actions/customer.ts
@@ -10,8 +10,11 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error("STRIPE_SECRET_KEY is not defined");
 }
 
+const stripeApiVersion =
+  process.env.STRIPE_API_VERSION ?? "2026-02-25.clover";
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2026-02-25.clover',
+  apiVersion: stripeApiVersion,
 });
 
 /**
